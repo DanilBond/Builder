9b5e7ecf96beeb9e7d89d763215535003d16b4b9
Elaman Imashov <sosgames75743@gmail.com>
2021-04-07 20:24:56 +0600

    Added link with firebase

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -11,17 +11,34 @@ import powersupply from "../../images/Icons/Powerblock.svg"
 import audiocard from "../../images/Icons/Audiocard.svg"
 import Componentitem from "../Componentitem/Componentitem";
 import PartEditor from "../PartEditor/PartEditor";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ComponentPart from "../Componentitem/ComponentPart";
+import axios from "axios";
+
 
 const Controls = () => {
+    let [isGettingData, getData] = useState(false)
     let [componentType, setComponentType] = useState("Exterior");
+    let [exteriorData, setExteriorData] = useState([]);
+    let [exterior, setExterior] = useState([]);
+
+    useEffect(
+        () => axios
+          .get('https://pcbuilder-989af-default-rtdb.firebaseio.com/series.json')
+          .then(response => {
+            setExteriorData(Object.values(response.data.cyber.parts.exterior));
+            for (const i of exteriorData) {
+                //exterior.push(i)
+                setExterior((oldItems) => [...oldItems, <ComponentPart price={i.price} name={i.name} classes={classes}/>]);
+            }
+            isGettingData = false;
+          }),[isGettingData]);
 
     return (
         <div className={classes.ControlPanels}>
             <div className={classes.Controls}>
                     
-                <div onClick={()=>{setComponentType("Exterior");}}>
+                <div onClick={()=>{setComponentType("Exterior"); getData(true);}}>
                     <Componentitem img={systemBlock} name="Exterior" classes={classes}/>
                 </div>
 
@@ -64,7 +81,7 @@ const Controls = () => {
                 </div>
 
                 <div onClick={()=>{}}>
-                    <ComponentPart price="1000 ₽" name="Red black" classes={classes}/>
+                    {exterior}
                 </div>
             </div>
         </div>
@@ -72,3 +89,12 @@ const Controls = () => {
 }
  
 export default Controls;
+
+// const result = [];
+//   for (const ingredient in ingredients) {
+//     for (let i = 0; i < ingredients[ingredient]; i++) {
+//       result.push(<PizzaIngredient key={ingredient + i} type={ingredient} />)
+//     }
+//   }
+
+//<ComponentPart price="1000 ₽" name="Red black" classes={classes}/>
