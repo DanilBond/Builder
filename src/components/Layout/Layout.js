508e70ee208ee64e016d408ff73add0459229dae
Elaman Imashov <sosgames75743@gmail.com>
2021-05-22 02:01:08 +0600

    Optimization test

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,7 +9,37 @@ import Price from "../Price/Price";
 import Modal from "../Modal/Modal";
 import Builder from "../Builder/Builder";
 import DrawerMobile from "../Drawer/DrawerMobile";
-import im from "../../images/Images/Cyber/CPU/i5.png"
+import i7 from "../../images/Images/Cyber/CPU/i7.png"
+import i9 from "../../images/Images/Cyber/CPU/i9.png"
+import bl from "../../images/Images/Cyber/Exterior/black.png"
+import yelb from "../../images/Images/Cyber/Exterior/blackyellow.png"
+import blre from "../../images/Images/Cyber/Exterior/bluered.png"
+import grr from "../../images/Images/Cyber/Exterior/green.png"
+import orr from "../../images/Images/Cyber/Exterior/orange.png"
+import pi from "../../images/Images/Cyber/Exterior/pink.png"
+import publ from "../../images/Images/Cyber/Exterior/purpleblue.png"
+import rebl from "../../images/Images/Cyber/Exterior/redblue.png"
+import wh from "../../images/Images/Cyber/Exterior/white.png"
+import g1660 from "../../images/Images/Cyber/GPU/1660ti.png"
+import g2060 from "../../images/Images/Cyber/GPU/2060.png"
+import g2070 from "../../images/Images/Cyber/GPU/2070.png"
+import g2080 from "../../images/Images/Cyber/GPU/2080ti.png"
+import g3070 from "../../images/Images/Cyber/GPU/3070.png"
+import g5500 from "../../images/Images/Cyber/GPU/5500.png"
+import g690 from "../../images/Images/Cyber/GPU/6900.png"
+import ex from "../../images/Images/Cyber/HDD/exos.png"
+import sea from "../../images/Images/Cyber/HDD/seagete.png"
+import skyh from "../../images/Images/Cyber/HDD/skyhawk.png"
+import z490 from "../../images/Images/Cyber/Mb/z470imsi.png"
+import z590 from "../../images/Images/Cyber/Mb/z590.png"
+import beq from "../../images/Images/Cyber/Power/bequite.png"
+import co1 from "../../images/Images/Cyber/Power/coolermaster.png"
+import co2 from "../../images/Images/Cyber/Power/coolermaster2.png"
+import cor from "../../images/Images/Cyber/Ram/corsair-RGB.png"
+import hyrgb from "../../images/Images/Cyber/Ram/hyperx-RGB.png"
+import hyx from "../../images/Images/Cyber/Ram/hyperx.png"
+import b1 from "../../images/Images/Cyber/Sound/b1.png"
+import b2 from "../../images/Images/Cyber/Sound/b2.png"
 
 
 const Layout = ({mouseData}) => {
@@ -80,7 +110,37 @@ const Layout = ({mouseData}) => {
         <Price price={price} setWindowState={setWindowState}  selected={selected} setOrderObj={setOrderObj} orderObj={orderObj}/>
         
         <Controls setImg={setImg} onStartLoadingEvent={onStartLoadingEvent} setOrderObj={setOrderObj} setSelected={setSelected}/>
-        <img src={im} alt=""/>
+        <img src={i7} alt=""/>
+        <img src={i9} alt=""/>
+        <img src={bl} alt=""/>
+        <img src={yelb} alt=""/>
+        <img src={blre} alt=""/>
+        <img src={grr} alt=""/>
+        <img src={orr} alt=""/>
+        <img src={pi} alt=""/>
+        <img src={publ} alt=""/>
+        <img src={rebl} alt=""/>
+        <img src={wh} alt=""/>
+        <img src={g1660} alt=""/>
+        <img src={g2060} alt=""/>
+        <img src={g2070} alt=""/>
+        <img src={g2080} alt=""/>
+        <img src={g3070} alt=""/>
+        <img src={g5500} alt=""/>
+        <img src={g690} alt=""/>
+        <img src={ex} alt=""/>
+        <img src={sea} alt=""/>
+        <img src={skyh} alt=""/>
+        <img src={z490} alt=""/>
+        <img src={z590} alt=""/>
+        <img src={beq} alt=""/>
+        <img src={co1} alt=""/>
+        <img src={co2} alt=""/>
+        <img src={cor} alt=""/>
+        <img src={hyrgb} alt=""/>
+        <img src={hyx} alt=""/>
+        <img src={b1} alt=""/>
+        <img src={b2} alt=""/>
     </div> );
 }
 
