83738e77b8cfa6d60ec24c98b2b955b733f9804c
Elaman Imashov <sosgames75743@gmail.com>
2021-05-22 13:46:12 +0600

    Bug fixes

--- src/components/Componentitem/ComponentCountable.js
+++ src/components/Componentitem/ComponentCountable.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
+
 const ComponentCountable = ({price, name, type, classes, url, setUrl, onStartLoadingEvent, setSelected}) => {
+    let [canStartLoad, setCanStartLoad] = useState(true);
+    let [count, setCount] = useState(1);
+    function disableMe(){
+        if(canStartLoad){
+            onStartLoadingEvent();
+        }
+        setCanStartLoad(false);
+    }
     return (
-        <div onClick={()=>{setUrl(url); onStartLoadingEvent(); setSelected({type:type, name:name, price:price});}}> 
+        <div onClick={()=>{setUrl(url); disableMe(); setSelected({type:type, name:name, price:parseInt(price)*count});}}> 
         <div className={classes.PartCC} >
             <div className={classes.ParentCC}>
             <span className={classes.NameCC}>{name}</span>
             <div className={classes.OneLineFlex}>
             <span className={classes.PriceCC}>{price}</span>
-            <input type="number" min="1" max="4"></input>
+            <input type="number" min="1" max="4" onChange={
+                 function(event){setCount(event.target.value);
+                 setSelected({type:type, name:name, price:parseInt(price)*event.target.value}); 
+                 console.log(parseInt(price)*event.target.value);
+                 if(event.target.value > 4){setSelected({type:type, name:name, price:parseInt(price)*4});
+                 event.target.value = 4;}
+                 if(event.target.value < 1){setSelected({type:type, name:name, price:parseInt(price)});
+                 event.target.value = 1;}
+                 }}/>
             </div>
             </div>
         </div>
--- src/components/Componentitem/ComponentPart.js
+++ src/components/Componentitem/ComponentPart.js
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
 
 const ComponentPart = ({price, name, type, classes, url, setUrl, onStartLoadingEvent, setSelected}) => {
-
+    let canStartLoad = true;
+    function disableMe(){
+        if(canStartLoad){
+            onStartLoadingEvent();
+        }
+        canStartLoad = false;
+    }
     return (
-        <div onClick={()=>{setUrl(url); onStartLoadingEvent(); setSelected({type:type, name:name, price:price}); }}> 
+        <div onClick={()=>{setUrl(url); disableMe(); setSelected({type:type, name:name, price:price}); }}> 
         <div className={classes.Part} >
             <div className={classes.ParentCP}>
             <span className={classes.NameCP}>{name}</span>
